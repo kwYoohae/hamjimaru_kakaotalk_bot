e507fbfd408ac35c57c43c5f7325d9c3348680d7
kwYoohae <gocksdl7979@gmail.com>
2021-07-01 22:47:13 +0900

    quickReply 구현

--- python_test/server_test.py
+++ python_test/server_test.py
@@ -3,22 +3,30 @@ import sys
 
 app = Flask(__name__)
 
+master_id = "118b05554dedc5781be3004fe1543e75c7f1058ee91418b6084955eff65ab71f79"
+message_block = "60d5e0792649813b4a32ec9a"
+
 @app.route('/keyboard')
 def Keyboard():
 	dataSend = {
 
 	}
 	return jsonify(dataSend)
-
+        
 @app.route('/message', methods=['POST'])
 def Message():
 	req = request.get_json()
-	content = req["action"]["detailParams"]["menu"]["value"]
+	content = req["userRequest"]["utterance"]
 	content=content.replace("\n","")
-	day_weeks = req["action"]["detailParams"]["day_of_the_week"]["value"]
-	print(content)
-	print(day_weeks)
+	id_value = req["userRequest"]["user"]["id"]
+	block_value = req["userRequest"]["block"]["id"]
+	answer_value = req["action"]["detailParams"]["answer_list"]["value"]
+	print(answer_value)
+	print(req["userRequest"]["utterance"])
+	print(id_value)
+	print(block_value)
 	if content == "오늘의 메뉴":
+		day_weeks = req["action"]["detailParams"]["day_of_the_week"]["value"]
 		dataSend = {
 			"version" : "2.0",
 			"template" : {
@@ -33,6 +41,37 @@ def Message():
 							}
 						]
 						}
+					}
+				]
+			}
+		}
+	elif content  == "이미지 업로드" and master_id == id_value: 
+		dataSend = {
+			"version" : "2.0",
+			"template" : {
+				"outputs" : [
+				{
+					"simpleText" : {
+						"text" : "이미지업로드 승인!!"
+						}
+					}
+				]
+			}
+		}
+	elif content == "명령어":
+		dataSend = {
+			"version" : "2.0",
+			"template" : {
+				"outputs" :[{
+					"simpleText":{
+						"text" : "메뉴를 눌러주세요"
+					}
+				}],
+				"quickReplies" : [
+					{
+						"action" : "message",
+						"label" : "오늘의 메뉴",
+						"messageText" : "메뉴"
 					}
 				]
 			}
