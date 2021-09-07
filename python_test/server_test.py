50bd1f21b1546834261729bda87736e18fc383f9
À¯ÇØÂù <gocksdl7979@gmail.com>
2021-09-07 23:08:50 +0900

    ✅ 주별 메뉴 선택 추가 및 csv파일 읽어오기

--- python_test/server_test.py
+++ python_test/server_test.py
@@ -1,95 +1,218 @@
 from flask import Flask, json, request, jsonify
 import sys
+import os
+import datetime
+import pandas as pd
 
 app = Flask(__name__)
 
-master_id = "118b05554dedc5781be3004fe1543e75c7f1058ee91418b6084955eff65ab71f79"
-message_block = "60d5e0792649813b4a32ec9a"
+menuData = pd.read_csv("./table.csv",  header = None, encoding = "utf=8")
+
+def get_img_content(coding='utf-8'):
+    with open('./TodayPicture/TodayMenu.png', 'rb') as f:
+        img_data = base64.b64encode(f.read()).decode(coding)
+        return img_data
+
 
 @app.route('/keyboard')
 def Keyboard():
-	dataSend = {
+    dataSend = {
+        'type': 'buttons',
+        'buttons': ['명령어']
+    }
+    return jsonify(dataSend)
 
-	}
-	return jsonify(dataSend)
-        
+
 @app.route('/message', methods=['POST'])
 def Message():
-	req = request.get_json()
-	content = req["userRequest"]["utterance"]
-	content=content.replace("\n","")
-	id_value = req["userRequest"]["user"]["id"]
-	block_value = req["userRequest"]["block"]["id"]
-	answer_value = req["action"]["detailParams"]["answer_list"]["value"]
-	print(answer_value)
-	print(req["userRequest"]["utterance"])
-	print(id_value)
-	print(block_value)
-	if content == "오늘의 메뉴":
-		day_weeks = req["action"]["detailParams"]["day_of_the_week"]["value"]
-		dataSend = {
-			"version" : "2.0",
-			"template" : {
-				"outputs" : [
-					{
-						"carousel" : {
-							"type" : "basicCard",
-							"items" : [
-							{
-								"title" : day_weeks ,
-								"description": "안뇨오옹"
-							}
-						]
-						}
-					}
-				]
-			}
-		}
-	elif content  == "이미지 업로드" and master_id == id_value: 
-		dataSend = {
-			"version" : "2.0",
-			"template" : {
-				"outputs" : [
-				{
-					"simpleText" : {
-						"text" : "이미지업로드 승인!!"
-						}
-					}
-				]
-			}
-		}
-	elif content == "명령어":
-		dataSend = {
-			"version" : "2.0",
-			"template" : {
-				"outputs" :[{
-					"simpleText":{
-						"text" : "메뉴를 눌러주세요"
-					}
-				}],
-				"quickReplies" : [
-					{
-						"action" : "message",
-						"label" : "오늘의 메뉴",
-						"messageText" : "메뉴"
-					}
-				]
-			}
-		}
-	else:
-		dataSend = {
-			"version" : "2.0",
-			"template" : {
-				"outputs" : [
-					{
-						"simpleText" : {
-							"text" : "error입니다."
-						}
-					}
-				]
-			}
-		}
-	return jsonify(dataSend)
+    req = request.get_json()
+    content = req["userRequest"]["utterance"]
+    content = content.replace("\n", "")
+    id_value = req["userRequest"]["user"]["id"]
+    block_value = req["userRequest"]["block"]["id"]
+    days = ['월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일']
+    dayweek = datetime.datetime.today().weekday()
+    # print(content)
+    if content == "월요일 메뉴":
+        day_weeks = req["action"]["detailParams"]["day_of_the_week"]["value"]
+        dataSend = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "carousel": {
+                            "type": "basicCard",
+                            "items": [
+                                {
+                                    "title": day_weeks,
+                                    "description": menuData[0][1]
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        }
+    elif content == "화요일 메뉴":
+        day_weeks = req["action"]["detailParams"]["day_of_the_week"]["value"]
+        dataSend = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "carousel": {
+                            "type": "basicCard",
+                            "items": [
+                                {
+                                    "title": day_weeks,
+                                    "description": menuData[1][1]
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        }
+    elif content == "수요일 메뉴":
+        day_weeks = req["action"]["detailParams"]["day_of_the_week"]["value"]
+        dataSend = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "carousel": {
+                            "type": "basicCard",
+                            "items": [
+                                {
+                                    "title": day_weeks,
+                                    "description": menuData[2][1]
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        }
+    elif content == "목요일 메뉴":
+        day_weeks = req["action"]["detailParams"]["day_of_the_week"]["value"]
+        dataSend = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "carousel": {
+                            "type": "basicCard",
+                            "items": [
+                                {
+                                    "title": day_weeks,
+                                    "description": menuData[3][1]
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        }
+    elif content == "금요일 메뉴":
+        day_weeks = req["action"]["detailParams"]["day_of_the_week"]["value"]
+        dataSend = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "carousel": {
+                            "type": "basicCard",
+                            "items": [
+                                {
+                                    "title": day_weeks,
+                                    "description": menuData[4][1]
+                                }
+                            ]
+                        }
+                    }
+                ]
+            }
+        }
+    elif content == "내일의 메뉴":
+        day_weeks = req["action"]["detailParams"]["day_of_the_week"]["value"]
+        if dayweek + 1 >= 6:
+            dataSend = {
+                "version": "2.0",
+                "template": {
+                    "outputs": [
+                        {
+                            "carousel": {
+                                "type": "basicCard",
+                                "items": [
+                                    {
+                                        "title": day_weeks,
+                                        "description": "안뇨오옹"
+                                    }
+                                ]
+                            }
+                        }
+                    ]
+                }
+            }
+    elif content == "오늘 밥은?":
+        dataSend = {
+            "version": "2.0",
+            "template": {
+                "outputs": [{
+                    "simpleText": {
+                        "text": "메뉴를 눌러주세요"
+                    }
+                }],
+                "quickReplies": [
+                    {
+                        "action": "message",
+                        "label": "월요일 메뉴",
+                        "messageText": "월요일 메뉴"
+                    },
+                    {
+                        "action": "message",
+                        "label": "화요일 메뉴",
+                        "messageText": "화요일 메뉴"
+                    },
+                    {
+                        "action": "message",
+                        "label": "수요일 메뉴",
+                        "messageText": "수요일 메뉴"
+                    },
+                    {
+                        "action": "message",
+                        "label": "목요일 메뉴",
+                        "messageText": "목요일 메뉴"
+                    },
+                    {
+                        "action": "message",
+                        "label": "금요일 메뉴",
+                        "messageText": "금요일 메뉴"
+                    }
+                ]
+            }
+        }
+    else:
+        dataSend = {
+            "version": "2.0",
+            "template": {
+                "outputs": [
+                    {
+                        "simpleText": {
+                            "text": "잘못된 입력입니다. 메뉴가 궁금하시면 다음버튼을 눌러주세요"
+                        }
+                    }],
+                "quickReplies": [
+                    {
+                        "action": "message",
+                        "label": "오늘 밥은?",
+                        "messageText": "오늘 밥은?"
+                    }
+                ],
+            }
+        }
+    return jsonify(dataSend)
+
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0', port=5000)
