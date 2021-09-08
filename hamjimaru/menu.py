f855c63ead5b6369696c884c199d5eaa3e6de5a3
Ubuntu <ubuntu@ip-172-31-32-58.us-east-2.compute.internal>
2021-09-08 15:00:36 +0000

    modify menu

--- hamjimaru/menu.py
+++ hamjimaru/menu.py
@@ -5,8 +5,12 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from html_table_parser import parser_functions as parser
 import pandas as pd
-import datetime
+from datetime import datetime
 from pandas import Series, DataFrame
+import schedule
+import time
+
+
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--no--sandbox')
 chrome_options.add_argument("--single-process")
@@ -22,50 +26,56 @@ def remove_residue(string):
 		removed += string[i]
 	return removed
 
-
+def job():
 # chromedriver 경로설정
-chromedriver = './chromedriver'
-driver = webdriver.Chrome(chromedriver, options=chrome_options)
-driver.implicitly_wait(1)
-driver.get('https://www.kw.ac.kr/ko/life/facility11.jsp')  # 스크래핑할 url 입력
-driver.implicitly_wait(1)
+	chromedriver = './chromedriver'
+	driver = webdriver.Chrome(chromedriver, options=chrome_options)
+	driver.implicitly_wait(1)
+	driver.get('https://www.kw.ac.kr/ko/life/facility11.jsp')  # 스크래핑할 url 입력
+	driver.implicitly_wait(1)
 
-table = driver.find_element_by_xpath("//*[@id=\"item_body\"]/div[3]/div/div[1]/article/div[3]/div/section/div/table")
-thead = table.find_elements_by_tag_name("thead")
-tbody = table.find_elements_by_tag_name("tbody")
+	table = driver.find_element_by_xpath("//*[@id=\"item_body\"]/div[3]/div/div[1]/article/div[3]/div/section/div/table")
+	thead = table.find_elements_by_tag_name("thead")
+	tbody = table.find_elements_by_tag_name("tbody")
 
-for tr in thead:
-	th= tr.find_elements_by_tag_name("th")
-	mon=th[1].text
-	tue=th[2].text
-	wed=th[3].text
-	thu=th[4].text
-	fri=th[5].text
+	for tr in thead:
+		th= tr.find_elements_by_tag_name("th")
+		mon=th[1].text
+		tue=th[2].text
+		wed=th[3].text
+		thu=th[4].text
+		fri=th[5].text
 
-#print("first command")
-#print('월:{0}, 화:{1}, 수:{2}, 목:{3}, 금:{4}'.format(mon,tue,wed,thu,fri))
-	diet1 =[mon, tue, wed, thu, fri]
-	m1={mon,tue,wed,thu,fri}    
-	
-for tr in tbody:
-	td= tr.find_elements_by_tag_name("td")
-	mon = remove_residue(td[1].text)
-	tue = remove_residue(td[2].text)
-	wed = remove_residue(td[3].text)
-	thu = remove_residue(td[4].text)
-	fri = remove_residue(td[5].text)
+	#print("first command")
+	#print('월:{0}, 화:{1}, 수:{2}, 목:{3}, 금:{4}'.format(mon,tue,wed,thu,fri))
+		diet1 =[mon, tue, wed, thu, fri]
+		m1={mon,tue,wed,thu,fri}    
+		
+	for tr in tbody:
+		td= tr.find_elements_by_tag_name("td")
+		mon = remove_residue(td[1].text)
+		tue = remove_residue(td[2].text)
+		wed = remove_residue(td[3].text)
+		thu = remove_residue(td[4].text)
+		fri = remove_residue(td[5].text)
 
 
-#print("second command")
-#print('월:{0}, 화:{1}, 수:{2}, 목:{3}, 금:{4}'.format(mon,tue,wed,thu,fri))
-	diet2 =[mon, tue, wed, thu, fri]
+	#print("second command")
+	#print('월:{0}, 화:{1}, 수:{2}, 목:{3}, 금:{4}'.format(mon,tue,wed,thu,fri))
+		diet2 =[mon, tue, wed, thu, fri]
 
-data= [diet1,diet2]
-toSave = pd.DataFrame(data)
-toSave.to_csv("./table.csv", index=False, header=False, encoding="utf-8")
+	data= [diet1,diet2]
+	toSave = pd.DataFrame(data)
+	toSave.to_csv("./table.csv", index=False, header=False, encoding="utf-8")
 
-now =datetime.datetime.now()
-with open("/log/log.txt",'a') as f:
-    f.write("["+now+"] "+"update the diet!")
+	now =datetime.now()
+	now = str(now)
+	with open("./logMenu.txt",'a') as f:
+		f.write("["+now+"] "+" update the diet!\n")
+	f.close()
+schedule.every(3).hours.do(job)
 
+while True:
+	schedule.run_pending()
+	time.sleep(1)
 driver.close()
